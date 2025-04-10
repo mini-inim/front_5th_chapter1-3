66e5deccc1f6b3c122827b14d12d35681ad7afbb
mini--inim <forestaroundseoul@gmail.com>
2025-04-11 00:51:17 +0900

    fix: memo.ts 업데이트

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, createElement, ReactNode } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
@@ -8,19 +8,21 @@ export function memo<P extends object>(
 ) {
   // 1. 이전 props를 저장할 ref 생성
   let oldProps: P | null = null;
-  let oldResult: JSX.Element;
+  let memorized: ReactNode | null = null;
 
   // 2. 메모이제이션된 컴포넌트 생성
-  function memoComponent(newProps: P) {
-    // 3. equals 함수를 사용하여 props 비교
-    if (_equals(newProps, oldProps)) {
-      return oldResult;
+  return function MemoizedComponent(props: P) {
+    if (oldProps === null || !_equals(oldProps, props)) {
+      
+      // 3. equals 함수를 사용하여 props 비교
+      memorized = createElement(Component, props);
+
+      // 4. props가 변경된 경우에만 새로운 렌더링 수행
+      oldProps = props;
     }
-
-    // 4. props가 변경된 경우에만 새로운 렌더링 수행
-    oldProps = newProps;
-    oldResult = React.createElement(Component, newProps);
-
-    return oldResult;
-  }
+    return memorized as ReactNode;
+  };
 }
+
+
+
